e7e439054513ce0e789ac511da57af45de9e4a44
alexander <kulnis71@gmail.com>
2022-10-02 19:31:27 +0300

    add connection to db, add new mo section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UtilsModule } from './modules/utils/utils.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { SectionsModule } from './modules/sections/sections.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
-    // TypeOrmModule.forRootAsync({
-    //   // imports: [ConfigModule],
-    //   // inject: [ConfigService],
-    //   // useFactory: async (config: ConfigService) => ({
-    //   //   type: config.get<'auroro-data-api'>('TYPEORM_CONNECTION'),
-    //   //   username: config.get<string>('TYPEORM_USERNAME'),
-    //   //   password: config.get<string>('TYPEORM_PASSWORD'),
-    //   //   database: config.get<string>('TYPEORM_DATABASE'),
-    //   //   port: config.get<number>('TYPEORM_PORT'),
-    //   //   entities: [__dirname + 'dist/**/*.entity{.ts,.js'],
-    //   //   synchronize: true,
-    //   //   autoLoadEntities: true,
-    //   //   logging: true,
-    //   // }),
-    //   // useFactory: async (config: ConfigService) => ({
-    //   //   type: config.get<'auroro-data-api'>('TYPEORM_CONNECTION'),
-    //   // }),
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
     UtilsModule,
     AuthModule,
     UsersModule,
+    SectionsModule,
   ],
   controllers: [],
   providers: [],
