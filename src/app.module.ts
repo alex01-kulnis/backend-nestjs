c868a4138dc01a1082f19a1e40b207188922ba08
alexander <kulnis71@gmail.com>
2022-10-02 15:17:12 +0300

    add auth entity, add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { UtilsModule } from './modules/utils/utils.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    // TypeOrmModule.forRootAsync({
+    //   // imports: [ConfigModule],
+    //   // inject: [ConfigService],
+    //   // useFactory: async (config: ConfigService) => ({
+    //   //   type: config.get<'auroro-data-api'>('TYPEORM_CONNECTION'),
+    //   //   username: config.get<string>('TYPEORM_USERNAME'),
+    //   //   password: config.get<string>('TYPEORM_PASSWORD'),
+    //   //   database: config.get<string>('TYPEORM_DATABASE'),
+    //   //   port: config.get<number>('TYPEORM_PORT'),
+    //   //   entities: [__dirname + 'dist/**/*.entity{.ts,.js'],
+    //   //   synchronize: true,
+    //   //   autoLoadEntities: true,
+    //   //   logging: true,
+    //   // }),
+    //   // useFactory: async (config: ConfigService) => ({
+    //   //   type: config.get<'auroro-data-api'>('TYPEORM_CONNECTION'),
+    //   // }),
+    // }),
+    UtilsModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
